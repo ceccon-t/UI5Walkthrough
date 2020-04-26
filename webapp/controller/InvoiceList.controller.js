0f7b55aeac016b2711eb573a6ca7f0d6727eca63
Tiago Ceccon <ceccon.t@hotmail.com>
2020-04-26 14:29:32 -0300

    Add step 32 code to pass parameters on navigation

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -34,8 +34,11 @@ sap.ui.define([
         },
 
         onPress: function(oEvent) {
+            let oItem = oEvent.getSource();
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("detail");
+            oRouter.navTo("detail", {
+                invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+            });
         }
 
     });
